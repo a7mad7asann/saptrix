94ed73370b1152a7eca2d3f8dbc2e452f4275de6
Ahmad Mohamed Embaby <133269149+a7mad7asann@users.noreply.github.com>
2023-09-11 23:48:55 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
+// Use jQuery to toggle the dropdown content
 $(document).ready(function(){
-  $('.open-menu-btn').click(function(){
-    $('.open-menu-btn').toggleClass('active')
-  })
+  $('.dropbtn').click(function(){
+      $('.dropdown-content').toggle();
+  });
+
+  // Close the dropdown when clicking outside of it
+  $(document).click(function(event){
+      if (!$(event.target).closest('.dropdown').length) {
+          $('.dropdown-content').hide();
+      }
+  });
 });
